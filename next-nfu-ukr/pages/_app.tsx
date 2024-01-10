9fbd7cb2a76b7c8bae558ce6c858cd2b3017ced7
Taras Ishchuk <58121868+ishchuktaras@users.noreply.github.com>
2024-01-10 18:38:14 +0000

    Add Web Analytics from Vercel

--- next-nfu-ukr/pages/_app.tsx
+++ next-nfu-ukr/pages/_app.tsx
@@ -2,12 +2,13 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { MoralisProvider } from 'react-moralis'
 import { NotificationProvider } from 'web3uikit'
-
+import { Analytics } from '@vercel/analytics/react'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NotificationProvider>
       <MoralisProvider initializeOnMount={false}>
         <Component {...pageProps} />
+        <Analytics />
       </MoralisProvider>
     </NotificationProvider>
   )
